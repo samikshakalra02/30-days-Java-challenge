3a259d3ca0d367e9eb8edf1edf0dfb43ae5282c1
samikshakalra02 <samikshakalra9811@gmail.com>
2026-01-17 21:11:20 +0530

    Create ScannerInput.java

---
+++ Day03/ScannerInput.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Scaninput
+  {
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter name");
+      String name;
+      name=sc.nextLine();
+      System.out.println("Enter age");
+      int age=sc.nextInt();
+      System.out.println("Enter marks");
+      float marks=sc.nextFloat();
+      System.out.println("User Deatils");
+      System.out.println("Name: "+name);
+      System.out.println("Age: "+age);
+      System.out.println("Marks: "+marks);
+    }
+  }
