f7c6846e6143bed3054ebb9bea9b3983de12490f
samikshakalra02 <samikshakalra9811@gmail.com>
2026-01-17 20:09:12 +0530

    Create Variables.java

---
+++ Day02/Variables.java
@@ -0,0 +1,16 @@
+public class Variables
+  {
+    public static void main(String args[])
+    {
+      int age=20;
+      float marks=97.5;
+      char grade='A';
+      boolean isStudent=true;
+      String name="Shubankar";
+      System.out.println("Name: "+name);
+      System.out.println("Is Student: "+isStudent);
+      System.out.println("Grade: "+grade);
+      System.out.println("Age: "+age);
+      System.out.println("Marks: "+marks);
+    }
+  }
